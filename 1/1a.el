f620928a933106d067eca688863d5ff6c2993e03
Dave Marquardt <davemarq@linux.ibm.com>
2024-12-04 13:40:55 -0600

    Fixes and improvements to 1a

--- 1/1a.el
+++ 1/1a.el
@@ -1,17 +1,18 @@
-(let 
+(let*
     (
-     (left (list))
-     (right (list))
+     left
+     right
      (buffer (find-file "input.txt"))
      (str (buffer-string))
      (lines (string-lines str))
-   )
+     )
   (dolist (l lines)
     (let (
 	  (values (split-string l))
 	  )
-      (setq left (append left (string-to-number (nth 0 values))))
-      (setq right (append right (string-to-number (nth 1 values))))
+      (setq left (append left (list (string-to-number (nth 0 values)))))
+      (setq right (append right (list (string-to-number (nth 1 values)))))
       )
     ;;; sort left and right numerically
+    )
   )
